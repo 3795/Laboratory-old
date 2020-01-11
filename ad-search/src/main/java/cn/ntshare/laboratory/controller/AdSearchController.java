3c685e3e552c5efb25f5a1c1f3b8b3f2e518863d
3795 <seven.iwi@gmail.com>
2020-01-11 17:28:26 +0800

    添加部分代码

--- ad-search/src/main/java/cn/ntshare/laboratory/controller/AdSearchController.java
+++ ad-search/src/main/java/cn/ntshare/laboratory/controller/AdSearchController.java
@@ -4,6 +4,11 @@ import cn.ntshare.laboratory.annotation.IgnoreResponseAdvice;
 import cn.ntshare.laboratory.client.SponsorClient;
 import cn.ntshare.laboratory.vo.AdPlan;
 import cn.ntshare.laboratory.vo.CommonResponse;
+import com.github.shyiko.mysql.binlog.BinaryLogClient;
+import com.github.shyiko.mysql.binlog.event.DeleteRowsEventData;
+import com.github.shyiko.mysql.binlog.event.EventData;
+import com.github.shyiko.mysql.binlog.event.UpdateRowsEventData;
+import com.github.shyiko.mysql.binlog.event.WriteRowsEventData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -55,5 +61,20 @@ public class AdSearchController {
     public CommonResponse<List<AdPlan>> feign(@RequestParam("userId") Long userId,
                                               @RequestParam("ids") List<Long> ids) {
         return sponsorClient.getAdPlanByIds(userId, ids);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BinaryLogClient client = new BinaryLogClient("127.0.0.1", 3306, "root", "root");
+        client.registerEventListener(event -> {
+            EventData data = event.getData();
+            if (data instanceof UpdateRowsEventData) {
+                System.out.println("Update: " + data.toString());
+            } else if (data instanceof WriteRowsEventData) {
+                System.out.println("Write: " + data.toString());
+            } else if (data instanceof DeleteRowsEventData) {
+                System.out.println("Delete: " + data.toString());
+            }
+        });
+        client.connect();
     }
 }
